e749fd1bacf4e7423c03acd95705eacad937014a
valeponick <vale.ponick@gmail.com>
2026-05-17 11:12:22 +0700

    feat: add SimpleQuiz with capitals and score

--- SimpleQuiz/main.swift
+++ SimpleQuiz/main.swift
@@ -7,5 +7,65 @@
 
 import Foundation
 
-print("Hello, World!")
+print("SimpleQuiz: ✅ Программа задаёт вопрос, user даёт ответ. Если ответ правильный — переходит к следующему вопросу. В конце показывает, сколько правильных ответов")
+  
+enum Command: String {
+    case start = "start"
+    case exit = "exit"
+    
+}
+ let capitals = [
+    "France": "Paris",
+    "Great Britain": "London",
+    "China": "Beijing",
+    "Spain": "Madrid",
+    "Chile": "Santiago",
+    "Germany": "Berlin",
+    "Italy": "Rome",
+    "USA": "Washington, D.C.",
+    "Canada": "Ottawa"
+]
+
+func startQuiz() {
+    print("Quiz started")
+    
+    var score = 0
+    
+    for (country, capital) in capitals {
+        print("> Capital of \(country)? ", terminator: "") // задаем вопрос
+        let input = readLine()? // получаем ответ
+            .trimmingCharacters(in: .whitespaces) ?? ""
+
+            if input.lowercased() == capital.lowercased() {
+                print("✅ Correct!")
+                score += 1
+            } else {
+                print("❌ Wrong. Correct answer: \(capital)")
+            }
+        }
+        print("🏆 Your score: \(score) out of \(capitals.count)")
+}
+    
+    while true {
+    print("\n> ", terminator: "") // приглашение в одной строке с вводом
+    guard let input = readLine()?
+        .trimmingCharacters(in: .whitespaces),
+          !input.isEmpty else { // защита от nil и пустого ввода
+        continue // при ошибке цикл повторяется, программа не падает
+    }
+        
+    guard let command = Command(rawValue: input) else { // разворачивает опционал, полученный из Command(rawValue: input)
+         print("Uncnown command") // Если команда неизвестна — ошибка и continue
+        continue
+        }
+        switch command {
+        case .start:
+            startQuiz()
+        case .exit:
+            print("By, bro!")
+            break
+        }
+        
+    }
+    
 
